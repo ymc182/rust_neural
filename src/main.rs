7b5f2a37a08068f025289702541c90c16dbe1c10
ymc182 <ymc182@gmail.com>
2022-12-22 12:17:28 +1100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 #[derive(Debug)]
 pub struct Neuron {
-    pub num: f64,
+    pub num: f32,
 }
 impl Neuron {
-    pub fn forward(&self) -> f32 {
-        let mut output = 0.0;
-
-        output
+    pub fn forward(&self, input: Vec<f32>) -> f32 {
+        0.0
     }
 }
 #[derive(Debug)]
@@ -14,10 +12,10 @@ pub struct Layer {
     pub neurons: Vec<Neuron>,
 }
 impl Layer {
-    pub fn forward(&self) -> Vec<f32> {
+    pub fn forward(&self, input: Vec<f32>) -> Vec<f32> {
         let mut output = Vec::new();
         for neuron in &self.neurons {
-            output.push(neuron.forward());
+            output.push(neuron.forward(input.clone()));
         }
         output
     }
@@ -25,7 +23,14 @@ impl Layer {
 #[derive(Debug)]
 pub struct Network {
     pub layers: Vec<Layer>,
-    pub forward: fn(Vec<f32>) -> Vec<f32>,
 }
-
+impl Network {
+    pub fn forward(&self, input: Vec<f32>) -> Vec<f32> {
+        let mut output = input;
+        for layer in &self.layers {
+            output = layer.forward(output);
+        }
+        output
+    }
+}
 fn main() {}
